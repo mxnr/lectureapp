cffb8a2e1329e3f9125611e83012f2496c70df56
Dmitry Semenov <mxnr@elance-odesk.com>
2015-09-24 21:06:56 +0300

    Added composer, index.php goes oop

---
+++ src/Feedler/Resources/feed.php
@@ -0,0 +1,5 @@
+<ol class="breadcrumb">
+    <li><a href="/">Главная</a></li>
+    <li class="active">Feed</li>
+</ol>
+<h1>КОТИКИ</h1>
---
+++ src/Feedler/Resources/index_page.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1>Привет, Feedler!</h1>
+        <p>
+            Много контента и котиков
+        </p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="/?page=feed" role="button">Перейти к просмотру</a>
+        </p>
+    </div>
+</div>
